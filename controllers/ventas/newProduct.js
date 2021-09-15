1bb1c1da9593dab019cc711b035ac16543064efb
Bernardo-Fraga <bernardo.fraga.angeloni@gmail.com>
2021-09-15 20:04:52 +0200

    Creado modulo newProduct. Falta por autentificar el Usuario en modulo users

--- controllers/ventas/newProduct.js
+++ controllers/ventas/newProduct.js
@@ -1,9 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////        SIN TERMINAR        ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
 /////////////////////////////////////////////////////////////////
 /// Función controladora para subir un nuevo producto a la BD ///
 /////////////////////////////////////////////////////////////////
 
+//const { th } = require('date-fns/locale'); // Y ESTO???????????????????? Apareció aquí hoy (15/9)
+
 // Necesitamos conectarnos a la BD (getDB)
 const getDB = require('../../ddbb/getDB.js');
+
+// Importamos la función que hicimos para formatear fechas desde helpers.js:
+const { formatDate } = require('../../helpers.js');
 
 // Ahora creamos la función asíncrona newProduct:
 const newProduct = async (req, res) => {
@@ -16,33 +25,17 @@ const newProduct = async (req, res) => {
 
     // Qué solicitamos para subir un nuevo producto??
     // Respuesta:
-    // idUser INT NOT NULL,
-    // FOREIGN KEY (idUser) REFERENCES users(id) ON DELETE CASCADE,
-    // nameProduct VARCHAR(50) NOT NULL,
-    // brand VARCHAR(50) NOT NULL,
-    // yearOfProduction VARCHAR(4),
-    // status ENUM('No funciona', 'A veces falla', 'Bien', 'Muy bien', 'Excelente') NOT NULL,
-    // category ENUM('Todas las Categorías', 'Ordenadores', 'Televisores', 'Telefonía', 'Música y Rádio', 'Consolas y Juegos') NOT NULL,
-    // description VARCHAR(200) NOT NULL,
-    // price MEDIUMINT NOT NULL,
-    // createdDate DATETIME NOT NULL,
-    // modifiedDate DATETIME,
-    // deletedDate DATETIME,
-    // active BOOLEAN DEFAULT true,
-    // sold BOOLEAN DEFAULT false,
-    // reserved BOOLEAN DEFAULT false,
-    // reservedDate DATETIME,
-    // deleted BOOLEAN DEFAULT false,
-    // chatRoom VARCHAR(250)
+    // idUser, brand, yearOfProduction, status, category, description, price, createdDate ,active
+
+    // De todos los datos de la BD, al usuario SOLO le vamos a pedir:
+    // idUser(***), nameProduct, brand, yearOfProduction, status, category, description y price.
 
     // (***) El idUser debemos obtenerlo del TOKEN, pero aún no hemos llegado a ese nivel...
     // así que vamos a hacer una trampita momentaneamente con el idUser, vamos a mandarlo
     // por el Body.
 
-    // De todos los datos de la BD, al usuario SOLO le vamos a pedir:
-    // idUser(***), nameProduct, brand, yearOfProduction, status, category, description y price.
-
-    // Obtenemos del Body (lo que nos envía el cliente en la REQUEST) los valores que necesitamos con destructuring:
+    // Obtenemos del Body (lo que nos envía el cliente en la REQUEST) los valores que
+    // necesitamos con destructuring:
     const {
       idUser,
       nameProduct,
@@ -54,10 +47,76 @@ const newProduct = async (req, res) => {
       price,
     } = req.body;
 
-    // Para confirmar si funciona: vamos a devolver (response) los mismos datos que pedimos:
-    res.send(req.body);
+    // Para confirmar que el usuario nos dé todos los datos que le pedimos, vamos a
+    // hacer que si faltan datos se lance un error:
+    if (
+      !idUser ||
+      !nameProduct ||
+      !brand ||
+      !yearOfProduction ||
+      !status ||
+      !category ||
+      !description ||
+      !price
+    ) {
+      throw new Error('Falta algún campo');
+    }
+
+    // Vamos a comprobar que el usuario ya exista en la BD (si no existe, no puede subir un producto):
+    const [user] = await connection.query(
+      `
+        SELECT * FROM users WHERE id = ?
+    `,
+      [idUser]
+    );
+    // Si no existe lanzamos un mensaje de error:
+    if (user.length < 1) {
+      throw new Error('El usuario seleccionado no existe');
+    }
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Si la acción llega hasta aquí es porque no hay errores, así que vamos a crear
+    // la entrada en la tabla productos:
+
+    // Para crear la entrada nos hace falta darle una fecha de creación (createdDate).
+    // Y tenemos que requerir la función que formatea la fecha (formatDate) de helpers.js
+    const createdDate = formatDate(new Date());
+    //
+    ////////////////////////////////////////////////////////////
+    /// Ahora sí definitivamente creamos la entrada a la BD: ///
+    ////////////////////////////////////////////////////////////
+    await connection.query(
+      `
+        INSERT INTO products (idUser, nameProduct, brand, yearOfProduction, status, category, description, price, createdDate, active)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `,
+      [
+        idUser,
+        nameProduct,
+        brand,
+        yearOfProduction,
+        status,
+        category,
+        description,
+        price,
+        createdDate,
+        true,
+      ]
+    );
+
+    // Para confirmar si funciona: vamos a devolver (response) un OBJETO con status:200
+    // y un mensaje de confirmación:
+    res.send({
+      status: 'Ok',
+      message: 'El producto ha sido creado correctamente',
+    });
   } catch (error) {
     console.error(error.message);
+    // Enviamos mensaje si el usuario no existe:
+    res.send({
+      status: 'error',
+      message: 'El usuario seleccionado no existe',
+    });
   } finally {
     // Pase lo que pase con la función, debemos liberar la conexión establecida:
     if (connection) connection.release();
