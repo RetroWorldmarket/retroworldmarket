06f71e61a0da686b05e7998a25094e1893bff7b0
Bernardo-Fraga <bernardo.fraga.angeloni@gmail.com>
2021-09-15 12:27:43 +0200

    descargado morgan y empezando los Endpoints

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-require('dontenv').config();
+require('dotenv').config();
 
 const express = require('express');
+
+// Requerimos la dependencia morgan, que sirve, básicamente,  para registrar
+// los detalles de las solicitudes al servidor. Es un "Logger".
+const morgan = require('morgan');
 
 const { PORT } = process.env;
 
 const app = express();
+
+// Usamos morgan como "Logger":
+app.use(morgan('dev'));
+
+// La información de las Request nos llegan a través del Body y, necesariamente,
+// tenemos que DESERIALIZARLO. Para ello usamos el método json de express:
+app.use(express.json());
+
+/////////////////////
+///// ENDPOINTS /////
+/////////////////////
+
+// Endpoint para subir un producto:
+//  POST /sellretro/-----> Botón PUBLICAR
 
 app.use((req, res) => {
   res.send('clarinete');
@@ -19,5 +37,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('conectado');
+  console.log(`Conectado al puerto: ${PORT}`);
 });
