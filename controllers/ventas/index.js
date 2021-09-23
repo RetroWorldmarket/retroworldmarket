60f5b6976efe67e888d029bb5ed8fbaefa564cad
james-23 <jaime.alex.adames@gmail.com>
2021-09-23 22:03:29 +0100

    Solucion de conflictos

--- controllers/ventas/index.js
+++ controllers/ventas/index.js
@@ -5,23 +5,17 @@ const deleteProduct = require('./deleteProduct');
 const sellRetro = require('./sellretro');
 const reservedProduct = require('./reservedRetro');
 const requestReserve = require('./requestReserve');
-<<<<<<< Updated upstream
 const deletePhoto = require('./deletePhoto');
-=======
 const rejectReserve = require('./rejectReserve');
->>>>>>> Stashed changes
 
 module.exports = {
-  addPhotoProduct,
-  editProduct,
-  newProduct,
-  deleteProduct,
-  sellRetro,
-  reservedProduct,
-  requestReserve,
-<<<<<<< Updated upstream
-  deletePhoto,
-=======
-  rejectReserve,
->>>>>>> Stashed changes
+    addPhotoProduct,
+    editProduct,
+    newProduct,
+    deleteProduct,
+    sellRetro,
+    reservedProduct,
+    requestReserve,
+    rejectReserve,
+    deletePhoto,
 };
