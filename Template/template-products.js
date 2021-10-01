2fba915661cf57bdff865f273eb946bfb6592a95
Bernardo-Fraga <bernardo.fraga.angeloni@gmail.com>
2021-10-01 14:33:19 +0200

    Agregando Template para subir fotos de productos

---
+++ Template/template-products.js
@@ -0,0 +1,17 @@
+// En este Template vamos a poner los datos de los distintos productos.
+
+// IMPORTANTE: usar el MISMO NOMBRE con que aparece en la Base de Datos!!!!!
+
+const cambiarNombre = [
+  {
+    idUser: '',
+    nameProduct: '',
+    brand: '',
+    yearOfProduction: '',
+    status: '',
+    category: 'cambiar nombre',
+    description: '',
+    price: '',
+    namePhoto: 'nombre de texto normal',
+  },
+];
