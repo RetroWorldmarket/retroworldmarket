094e005f97eb6feb94e1d47ffbf7029a6fa5f539
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2021-10-09 18:16:26 +0200

    Co-authored-by: Anaevee <Anaevee@users.noreply.github.com> Co-authored-by: Bernardo-Fraga <Bernardo-Fraga@users.noreply.github.com>

--- controllers/compras/index.js
+++
@@ -1,9 +0,0 @@
-const categoryProduct = require('./categoryProduct.js');
-const getProduct = require('./getproduct.js');
-const search = require('./search.js');
-
-module.exports = {
-  categoryProduct,
-  search,
-  getProduct,
-};
--- controllers/mensajes/index.js
+++
@@ -1,9 +0,0 @@
-const sendMessage = require('./sendMessage.js');
-const getMessage = require('./getMessage.js');
-const responseMessage = require('./responseMessage.js');
-
-module.exports = {
-  sendMessage,
-  getMessage,
-  responseMessage,
-};
