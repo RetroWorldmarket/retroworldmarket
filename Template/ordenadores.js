da0d9ffa825ffeecd481dcd5ed60eb6f3666e03d
Anaevee <evee_mcm@hotmail.com>
2021-10-09 15:12:23 +0200

    ordenadores

--- Template/ordenadores.js
+++ Template/ordenadores.js
@@ -1,14 +1,149 @@
-const cambiarNombre = [
+const ordenadores = [
     {
-      nameProduct: 'Playstation',
-      brand: 'Sony',
+      nameProduct: 'ordenador',
+      brand: 'toshiba',
       yearOfProduction: 2002,
-      category: 'Consolas y Juegos',
-      description: 'Consola clásica / (Lorem Ipsum)',
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
       price: '80',
       namePhoto: {
-        1: 'Consolas1-1.jpg',
-        2: 'Consolas1-2.jpg',
+        1: '1.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'Hewlett-Packard 100LX ',
+      yearOfProduction: 1993,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '100',
+      namePhoto: {
+        1: '2.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'apple ',
+      yearOfProduction: 2002,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '120',
+      namePhoto: {
+        1: '3.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'sony ',
+      yearOfProduction: 1998,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '150',
+      namePhoto: {
+        1: '4.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'ibm personal ',
+      yearOfProduction: 1981,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '130',
+      namePhoto: {
+        1: '5.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'columbia data ',
+      yearOfProduction: 1982,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '160',
+      namePhoto: {
+        1: '6.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'sony ',
+      yearOfProduction: 2000,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '145',
+      namePhoto: {
+        1: '7.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'apple ',
+      yearOfProduction: 2001,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '155',
+      namePhoto: {
+        1: '8.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'ibm ',
+      yearOfProduction: 1999,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '165',
+      namePhoto: {
+        1: '9.jpg',
+      
+      },
+    },
+  ]; 
+
+  const ordenadores = [
+    {
+      nameProduct: 'ordenador',
+      brand: 'toshiba ',
+      yearOfProduction: 2005,
+      category: 'ordenadores',
+      description: 'ordenador clasico / (Lorem Ipsum)',
+      price: '175',
+      namePhoto: {
+        1: '10.jpg',
+      
       },
     },
   ]; 
