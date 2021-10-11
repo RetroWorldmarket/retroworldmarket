2ad277f4ca30b14b3e4e9832b2deb9ef17fd4abf
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2021-10-11 10:48:02 +0200

    get product ok

--- backend/controllers/compras/getproduct.js
+++ backend/controllers/compras/getproduct.js
@@ -6,35 +6,38 @@ const getProduct = async (req, res, next) => {
   try {
     connection = await getDB();
 
-    const {idProduct} = req.params;
+    const { idProduct } = req.params;
 
-    const [getProduct] = await connection.query(`
+    const [getProduct] = await connection.query(
+      `
     SELECT products.id, products.idUser , products.nameProduct , products.brand , products.yearOfProduction , products.status, products.category , products.description , products.price, AVG(IFNULL(votes.vote, 0)) AS votes
     FROM products 
     LEFT JOIN  votes ON (products.idUser = votes.idUser)
     WHERE products.id = ?
 	GROUP BY products.id
 
-    `, [idProduct]);
-  
-  const [foto] = await connection.query(`
+    `,
+      [idProduct]
+    );
+
+    const [fotos] = await connection.query(
+      `
   SELECT namePhoto
   FROM photos
   WHERE idProduct = ?
 
-  `
+  `,
 
-  , [ idProduct]);
+      [idProduct]
+    );
 
-  const data = [...getProduct, foto]
-  console.log(data);
-
-  res.send({
-    status: "ok",
-    data
-
-  })
-
+    res.send({
+      status: 'ok',
+      producto: {
+        ...getProduct,
+        fotos,
+      },
+    });
   } catch (error) {
     next(error);
   } finally {
