07aeef8d6e9f53eb82ce47252d56b8ddb36265f1
james-23 <jaime.alex.adames@gmail.com>
2021-10-22 16:19:17 +0100

    Subiendo Repo Css sin terminar

--- react/retro/src/App.js
+++ react/retro/src/App.js
@@ -3,17 +3,17 @@ import { Inicio } from './components/Inicio';
 import { InicioHeader } from './components/InicioHeader';
 import { PlusArticuloModal } from './components/articulo/PlusArticuloModal';
 import { OrdenarPor } from './components/ordenarPor/OrdenarPor';
-// import Modales from './components/Modales';
+import Modales from './components/Modales';
 
 function App() {
-  return (
-    <div className='App'>
-      <InicioHeader />
-      <OrdenarPor />
-      <PlusArticuloModal />
-      <Inicio />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <InicioHeader />
+            <OrdenarPor />
+            <PlusArticuloModal />
+            <Inicio />
+        </div>
+    );
 }
 
 export default App;
