b086b8ca2bdc37e3e256379564182be7186b6eb7
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2021-10-26 18:36:57 +0200

    arregladas fotos

--- backend/ddbb/initDB.js
+++ backend/ddbb/initDB.js
@@ -313,8 +313,8 @@ const prod = [
     description: 'Consola clásica / (Lorem Ipsum)',
     price: '150',
     namePhoto: {
-      1: 'consola2-1.jpeg',
-      2: 'consola2-2.jpeg',
+      1: 'consolas2-1.jpeg',
+      2: 'consolas2-2.jpeg',
     },
   },
   {
@@ -325,8 +325,8 @@ const prod = [
     description: 'Consola clásica con 64 juegos',
     price: '100',
     namePhoto: {
-      1: 'consola3-1.jpeg',
-      2: 'consola3-2.jpeg',
+      1: 'consolas3-1.jpeg',
+      2: 'consolas3-2.jpeg',
     },
   },
   {
@@ -337,8 +337,8 @@ const prod = [
     description: 'Consola clásica / (Lorem Ipsum)',
     price: '80',
     namePhoto: {
-      1: 'consola4-1.jpeg',
-      2: 'consola4-2.jpeg',
+      1: 'consolas4-1.jpeg',
+      2: 'consolas4-2.jpeg',
     },
   },
   {
@@ -349,8 +349,8 @@ const prod = [
     description: 'Consola clásica con juego',
     price: '100',
     namePhoto: {
-      1: 'consola5-1.jpeg',
-      2: 'consola5-2.jpeg',
+      1: 'consolas5-1.jpeg',
+      2: 'consolas5-2.jpeg',
     },
   },
   {
@@ -361,8 +361,8 @@ const prod = [
     description: 'Consola clásica con 2 mandos',
     price: '90',
     namePhoto: {
-      1: 'consola6-1.jpeg',
-      2: 'consola6-2.jpeg',
+      1: 'consolas6-1.jpeg',
+      2: 'consolas6-2.jpeg',
     },
   },
   {
@@ -373,8 +373,8 @@ const prod = [
     description: 'Consola histórica con 2 mandos y juegos',
     price: '500',
     namePhoto: {
-      1: 'consola7-1.jpeg',
-      2: 'consola7-2.jpeg',
+      1: 'consolas7-1.jpeg',
+      2: 'consolas7-2.jpeg',
     },
   },
   {
@@ -385,8 +385,8 @@ const prod = [
     description: 'Consola clásica de viaje con juegos',
     price: '50',
     namePhoto: {
-      1: 'consola8-1.jpeg',
-      2: 'consola8-2.jpeg',
+      1: 'consolas8-1.jpeg',
+      2: 'consolas8-2.jpeg',
     },
   },
   {
@@ -397,8 +397,8 @@ const prod = [
     description: 'Consola clásica con 2 mandos',
     price: '90',
     namePhoto: {
-      1: 'consola9-1.jpeg',
-      2: 'consola9-2.jpeg',
+      1: 'consolas9-1.jpeg',
+      2: 'consolas9-2.jpeg',
     },
   },
   {
@@ -409,8 +409,8 @@ const prod = [
     description: 'Consola clásica de viaje',
     price: '50',
     namePhoto: {
-      1: 'consola10-1.jpeg',
-      2: 'consola10-2.jpeg',
+      1: 'consolas10-1.jpeg',
+      2: 'consolas10-2.jpeg',
     },
   },
   {
