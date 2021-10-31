3b06ce0bb5a92f724b163d47060b5f63a8301451
Anaevee <evee_mcm@hotmail.com>
2021-10-31 20:03:44 +0100

    paginasdecatalogos terminadas a falta de ordenar por

--- react/retro/src/components/galeriaProductos/GaleriaProductos.js
+++ react/retro/src/components/galeriaProductos/GaleriaProductos.js
@@ -17,26 +17,34 @@ export const GaleriaProductos = () => {
   let query = useQuery();
   const [PeticionPorCategorias, setPeticionPorCategorias] = useState([]);
   const [PeticionSearch, setPeticionSearch] = useState([]);
+  const [MasBuscado,setMasBuscado] = useState([]);
+  const [LoUltimo,setLoUltimo]= useState([]);
   const categorias = query.get('category');
   const search = query.get('search');
 
   useEffect(() => {
-    if (search === null) {
+    if (categorias) {
       get(`http://localhost:4000/category?category=${categorias}`, (body) => {
         setPeticionPorCategorias(body.data.results);
         setPeticionSearch([]);
       });
-    } else {
+    } else if (search) {
       get(`http://localhost:4000/search?search=${search}`, (body) => {
         setPeticionSearch(body.data.results);
-      });
-    }
-  }, [categorias, search]);
+      })} else {
+        get(`http://localhost:4000/inicio`, (body) => {setMasBuscado (body.productos)});
+        get(`http://localhost:4000/inicio`, (body) => {setLoUltimo (body.productos)});
+        
 
+      }
+      
+    
+  }, [categorias, search]);
+  
   return (
     <div>
+      {(search || categorias ) && 
       <div className='centrado'>
-        <h3>Lo último en llegar...</h3>
         <nav className='nav-galeria'>
           {PeticionSearch.length > 0 &&
             PeticionSearch.map((art) => {
@@ -61,17 +69,46 @@ export const GaleriaProductos = () => {
             })}
         </nav>
       </div>
+          } :
+      {(!categorias && !search) && <section>
+         <div className='centrado'>
+         <h3>Lo último en llegar...</h3>
+         <nav className='nav-galeria'>
+         {LoUltimo.length > 0 &&
+            LoUltimo.map((art) => {
+              return (
+                <TarjetaArticulo
+                  id='TarjetaArticulo'
+                  key={art.id}
+                  articulo={art}
+                />
+              );
+            })}
+         </nav>
+         <button>
+           <Link to='/'>Volver al inicio</Link>
+         </button>
+       </div>
       <div className='centrado'>
         <h3>Lo más buscado...</h3>
         <nav className='nav-galeria'>
-          {/* <TarjetaArticulo />
-          <TarjetaArticulo />
-          <TarjetaArticulo /> */}
+        {MasBuscado.length > 0 &&
+            MasBuscado.map((art) => {
+              return (
+                <TarjetaArticulo
+                  id='TarjetaArticulo'
+                  key={art.id}
+                  articulo={art}
+                />
+              );
+            })}
         </nav>
         <button>
           <Link to='/'>Volver al inicio</Link>
         </button>
       </div>
+      </section>
+}
       <Footer />
     </div>
   );
